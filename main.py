e1b0166f370327b2a73f29f3541ebc8147c0cebd
Jurassic001 <mhaberer25@gmail.com>
2024-01-06 03:07:08 -0600

    Got some more testing to do but we're approaching the finished (not final ;)) producf

--- main.py
+++ main.py
@@ -15,13 +15,12 @@ def targFolder():
 
 def fileExt(formatCode: int):
     """
-    Format codes: 0 - terminal format, 1 - display format, 2 - display opposite format
+    Format codes: 0 - display format, 1 - display opposite format
     """
     ext = ['mp4','mp3']
     if isVideo and ext[0] == 'mp3': ext.reverse()
     elif not isVideo and ext[0] == 'mp4': ext.reverse()
-    if not formatCode: return '-f ' + ext[0]
-    return ext[formatCode - 1]
+    return ext[formatCode]
 
 def processURL():
     for i in range(3):
@@ -32,34 +31,30 @@ def processURL():
             print(processingMsg)
             time.sleep(0.5)
 
-def downloadVideo(link):
+def downloadVideo(link, vidName):
     # Print the name of the to-be downloaded video in a more readable format
-    print("")
-    subprocess.run(['yt-dlp', '-ODownloading: "%(title)s" to your' + targFolder() + 'folder', link])
+    clear()
+    print("Downloading: " + vidName + " to your " + targFolder() + " folder")
     print("")
     # Let the user (me) read the printed line before filling the console with status updates
     time.sleep(1)
     # Start downloading the video
-    """All of the below commands work, but they all seem to max out at 720p video quality"""
-    # subprocess.run(['yt-dlp','--ffmpeg-location','C:/FFmpeg/bin/ffmpeg.exe','-S res:1080','-o~/Videos/%(title)s.%(ext)s', link])
-
-    subprocess.run(['yt-dlp', fileExt(0),'-o~/' + targFolder() + '/%(title)s.%(ext)s', link])
-
-    # subprocess.run(['yt-dlp','-f mp4','-o~/Videos/%(title)s.%(ext)s', link])
-    # subprocess.run(['yt-dlp','--ffmpeg-location','C:/FFmpeg/bin/ffmpeg.exe','-x','--audio-format','mp3','-o~/Videos/%(title)s.%(ext)s', link])
-    """"""
-    # Give the user (again, me) time to process a succesful or failed download
+    if isVideo:
+        subprocess.run(['yt-dlp', '-f "bv*[vcodec^=avc]+ba[ext=m4a]/b[ext=mp4]/b"', '-o~/Videos/%(title)s.%(ext)s', link])
+    else:
+        subprocess.run(['yt-dlp','--ffmpeg-location','C:/FFmpeg/bin/ffmpeg.exe','-x','--audio-format','mp3','-o~/Music/%(title)s.%(ext)s', link])
+    # Give the user time to process a succesful or failed download
     time.sleep(0.5)
 
 def configDownload(link):
     global isVideo
     dlPhase = 2
     processing.start()
-    videoName = subprocess.check_output(['yt-dlp', '-OSelected video: %(title)s', link], text=True, timeout=5)
+    videoName = subprocess.check_output(['yt-dlp', '-O%(title)s', link], text=True, timeout=5)
     processing.join()
     while dlPhase == 2:
         clear()
-        print(videoName)
+        print("Selected video: " + videoName)
         print("Downloading as an " + fileExt(1))
         print("Press M to switch to " + fileExt(2))
         print("")
@@ -67,7 +62,7 @@ def configDownload(link):
         print("Press Backspace to select another video")
         time.sleep(0.5)
         while True:
-            if kb.is_pressed("enter"): downloadVideo(link)
+            if kb.is_pressed("enter"): downloadVideo(link, videoName)
             elif kb.is_pressed("backspace"): 
                 dlPhase = 1
                 break
