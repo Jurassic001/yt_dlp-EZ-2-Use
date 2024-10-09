9d281e63e22e46770c111751a979c1eb47b061d8
Max Haberer <mhaberer25@gmail.com>
2024-10-09 12:21:30 -0500

    Refactor input handling and add skip prompts feature

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
+import argparse
 import os
 import subprocess
 import sys
 import time
 
-import keyboard as kb
 import pyperclip
 
 
 class simple_ytdl:
-    def __init__(self) -> None:
+    def __init__(self, skip: bool, skip_val: str) -> None:
         self.EXT_DICT: dict[bool, str] = {
             True: "mp4",
             False: "mp3",
@@ -19,13 +19,25 @@ class simple_ytdl:
             subprocess.CalledProcessError: "Invalid URL or the video cannot be found.",
         }  # Dictionary of error messages that correspond to the exception
 
-        self.clear = lambda: os.system("cls")  # Clear the console
+        self.clear = lambda: os.system("cls" if os.name == "nt" else "clear")  # Clear the console
         self.isVideo: bool = True  # Download format: True - mp4, False - mp3
 
-        if sys.platform.startswith("linux"):
-            self.paste = lambda: subprocess.check_output("xclip -selection clipboard -o", shell=True).decode().strip()
+        self.skip_prompts: bool = skip  # Skip input prompts
+        self.prompt_skip_val: str = skip_val  # Value to return when skipping prompts
+
+    def input(self, prompt: str = "") -> str:
+        """Prompt the user for input, or skip the prompt if the relevant argument is applied
+
+        Args:
+            prompt (str): The prompt to display to the user
+
+        Returns:
+            str: The user's input, or the value returned when skipping prompts
+        """
+        if self.skip_prompts:
+            return self.prompt_skip_val
         else:
-            self.paste = pyperclip.paste
+            return input(prompt).strip().lower()
 
     def downloadVideo(self, link: str, vidName: str) -> None:
         """Download the target video
@@ -34,7 +46,7 @@ class simple_ytdl:
             link (str): The URL of the media that you want to download
             vidName (str): The name of the media that you want to download
         """
-        # Print the name of the to-be downloaded video and it's destination
+        # Print the name of the to-be downloaded video and its destination
         self.clear()
         targ_folder = "Videos" if self.isVideo else "Music"
         print(f"Downloading: {vidName} to your {targ_folder} folder\n")
@@ -67,9 +79,8 @@ class simple_ytdl:
                 ]
             )
         # Make sure the user sees the successful/failed download status before closing the console.
-        print("\nPress Enter to exit the program")
-        kb.wait("Enter")
-        sys.exit()
+        self.input("\nPress Enter to exit the program")
+        sys.exit(0)
 
     def configDownload(self, link: str) -> None:
         """Here the user can see the video title, choose to download as an mp4 or mp3, and continue or go back
@@ -78,37 +89,34 @@ class simple_ytdl:
             link (str): The URL of the media that you want to download
         """
         self.clear()
-        print("Processing the URL...\n")
+        print("Processing the URL...", end="\n\n")
         try:
             videoName = (subprocess.check_output(["yt-dlp", "-O", "%(title)s", link], text=True, timeout=10)).strip()
         except Exception as e:
             err_msg = self.ERROR_MSG.get(type(e), "An unknown error occurred.")
-            print(f"\n{err_msg} Press Enter to try again.")
-            kb.wait("enter")
+            self.input(f"\n{err_msg} Press Enter to try again. ")
             return
         while True:
             self.clear()
             print(f"Selected video: {videoName}")
-            print(f"Downloading as an {self.EXT_DICT[self.isVideo]}")
-            print(f"Press M to switch to {self.EXT_DICT[not self.isVideo]}\n")
-            print("Press Enter to start the download")
-            print("Press Backspace to select another video")
+            print(f"Downloading as an {self.EXT_DICT[self.isVideo]}", end="\n\n")
+
+            print("Type anything to confirm and download the video")
+            print(f'Type "S" to switch to {self.EXT_DICT[not self.isVideo]}')
+            print('Type "N" to go back')
             time.sleep(0.5)
-            while True:
-                if kb.is_pressed("enter"):
-                    self.downloadVideo(link, videoName)
-                elif kb.is_pressed("m"):
-                    self.isVideo = not self.isVideo
-                    break
-                elif kb.is_pressed("backspace"):
-                    return
+            user_input = self.input()
+            if user_input.startswith("s"):
+                self.isVideo = not self.isVideo
+            elif user_input.startswith("n"):
+                return
+            else:
+                self.downloadVideo(link, videoName)
 
     def main(self) -> None:
         while True:
             # Print instructions and wait for user input
             self.clear()
-            print("Press Enter once you've copied the URL")
-            kb.wait("Enter")
             if sys.platform == "win32":
                 url = pyperclip.paste()
             elif sys.platform == "linux":
@@ -122,17 +130,30 @@ class simple_ytdl:
             if url.startswith("http"):
                 self.configDownload(url)
             else:
-                # Warn the user if a URL isn't detected and give them the option to abandon/continue the download.
-                print("\nValid URL not found. Do you want to continue? (Yes/Y/Enter or No/N/Backspace)")
+                # Warn the user if a URL isn't detected and give them the option to continue/retry/abandon the download.
+                valid_fail = self.input("\nValid URL not found. Do you want to continue? (y/n) ")
                 time.sleep(0.5)
-                while True:
-                    if kb.is_pressed("y") or kb.is_pressed("enter"):
-                        self.configDownload(url)
-                        break
-                    elif kb.is_pressed("n") or kb.is_pressed("backspace"):
-                        break
+                if valid_fail.startswith("n"):
+                    continue
+                else:
+                    self.configDownload(url)
 
 
 if __name__ == "__main__":
-    download = simple_ytdl()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--skip",
+        action="store_true",
+        default=False,
+        help="Skip input prompts and use default values",
+    )
+    parser.add_argument(
+        "--skip-val",
+        type=str,
+        default="",
+        help="Value to return when skipping prompts",
+    )
+    args = parser.parse_args()
+
+    download = simple_ytdl(args.skip, args.skip_val)
     download.main()
